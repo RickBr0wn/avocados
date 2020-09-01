bdb145d31819f7b7e55438fde14235edd354f4a0
Rick Brown <ricky.brown.00@gmail.com>
2020-09-02 00:29:25 +0100

    adds header

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Avocado Reciepes")
-    }
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack(alignment: .center, spacing: 20) {
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(alignment: .top, spacing: 0) {
+            HeaderView()
+          }
+        }
+        VStack(alignment: .center, spacing: 20) {
+          Text("All about avocados")
+            .font(.system(.title, design: .serif))
+            .fontWeight(.bold)
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
+          Text("Everything you wanted to know about avocados but were to afraid to ask.")
+            .font(.system(.body, design: .serif))
+            .multilineTextAlignment(.center)
+            .foregroundColor(Color.gray)
+            .frame(maxWidth: 340)
+        }
+        .frame(maxWidth: 640)
+        .padding()
+        .padding(.bottom, 85)
+      }
+    }a
+    .edgesIgnoringSafeArea(.all)
+    .padding(0)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
